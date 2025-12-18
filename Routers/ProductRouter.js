6d5149d51d35e7362ceab1dda478d2238eb194d7
Kanika-0905 <kanikakaruppusamy09@gmail.com>
2025-12-18 08:49:38 +0530

    Delete Routers directory

--- Routers/ProductRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {getAllProducts, addProduct, updateProduct, deleteProduct} = require("./../Controllers/ProductController");
-
-router.get("/", getAllProducts);
-
-module.exports = router;
